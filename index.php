1ed737a6ce5e1ec7db7b2de6c6a0c53a0106a1f4
scottdevin <laconescott@gmail.com>
2015-07-25 12:08:53 -0400

    'hi'

---
+++ index.php
@@ -0,0 +1,229 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8">
+<meta name="description" content="Taemoney">
+<meta name="keywords" content="music">
+<meta name="author" content="Devin Scott">
+
+
+<meta name='viewport' content='width=device-width,initial-scale=1'>
+    <title>Example document</title>
+	<!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+<link rel="stylesheet" type="text/css" href="http://cdn.jsdelivr.net/jquery.slick/1.5.0/slick.css"/>
+<!-- jQuery library -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+
+<!-- Latest compiled JavaScript -->
+<script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
+<link href="style.css" rel="stylesheet">
+  </head>
+  <body>
+  <nav class="navbar">
+  <img src="http://s15.postimg.org/ofpo5jm2h/kosagang_white.png" class="logo"/>
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span> 
+      </button>
+    </div>
+    <div class="collapse navbar-collapse" id="myNavbar">
+      <ul class="nav navbar-nav">
+        <li><a href="#home">HOME</a></li>
+        <li><a href="#music">MUSIC</a></li>
+        <li><a href="#video">VIDEOS</a></li> 
+        <li><a href="#contact">CONTACT</a></li> 
+      </ul>
+      <ul class="nav navbar-nav navbar-right">
+        <li><a href="https://twitter.com/taemoney419"><i class="fa fa-twitter-square fa-lg"></i></a></li>
+        <li><a href="https://www.youtube.com/user/realtaemoney"><i class="fa fa-youtube-square fa-lg"></a></i>
+</li>
+	
+		
+      </ul>
+    </div>
+  </div>
+</nav>
+  
+  
+<div class="container" id="home">
+<div class="row">
+<div class="col-md-4 col-md-offset-4 ">
+<img src="http://s13.postimg.org/bmj1cc1ol/taemoneyartworkface.png" class="tae img-responsive">
+</div></div>
+<div class="row">
+<div class="col-md-4 col-md-offset-4 col-xs-offset-1">
+<img class="tae2" src="http://s23.postimg.org/6zbn63cwp/Untitled_3.png"/>
+</div></div>
+<div class="row">
+<div class="col-md-12">
+<h2 class="text">Tae Money is an upcoming artist from Toledo Ohio. Grownig up in the 90's, he was highly influenced by popular artists at the time such as 
+<a href="https://en.wikipedia.org/wiki/The_Notorious_B.I.G." class="j">The Notorious B.I.G, </a><a href="https://en.wikipedia.org/wiki/Cash_Money_Records" class="j">
+Cash Money Records, <a href="https://en.wikipedia.org/wiki/Dr._Dre" class="j">Dr. Dre</a>, and more. His aspirations of becoming a rapper started very
+ early in his life, at the age of 11 he was selling his music on cassette tapes to his friends. </h2>
+ </div></div>
+ 
+ 
+ 
+<div class="f fadeInBlock">
+   <div><img class="z" src="http://images.vectorhq.com/images/previews/6bd/taylor-gang-2-psd-442605.png" alt=""></a></div>
+  <div><img class="z" src="http://www.ymcmb.fr/images/defautvideo.png" alt=""></a></div>
+  <div><img class="z" src="http://s16.postimg.org/qzhtgrnl1/1017_Brick_Squadd.png6" alt=""></a></div>
+    </div>
+
+    <script type="text/javascript" src="http://code.jquery.com/jquery-1.11.0.min.js"></script>
+    <script type="text/javascript" src="http://code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
+<script type="text/javascript" src="http://cdn.jsdelivr.net/jquery.slick/1.3.11/slick.min.js"></script>
+
+
+    <script type="text/javascript">
+        $(document).ready(function(){       
+$('.f').slick({
+    dots: false,fade:false,
+    infinite: true,
+    speed: 700,
+    autoplay:true,
+    autoplaySpeed: 2000,
+    arrows:false,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+	pauseOnHover:false,
+	pauseOnDotsHover:true
+ });
+    
+        });
+</script>
+
+<div class="row" id="music">
+<div class="col-md-12">
+<h1 class="text2">Not only is Taemoney a rapper, he is also a gifted producer.
+His discography includes producing for the above.</h1>
+</div>
+</div>
+<div class="row">
+<div class="col-md-4 col-md-offset-4 ">
+<div class="s" style="width:380px; height:170px; overflow:hidden; position:relative"><div><embed src="http://www.soundclick.com/widgets/creatives/mp3PlayerVIP.swf" width="380" height="170" flashvars="bandid=1112648&ext=1" name="MP3PlayerVIP" type="application/x-shockwave-flash" wmode="transparent" pluginspage"http://www.macromedia.com/go/getflashplayer" /></div><div style="width:150px; position:relative; top:-170px; height:170px; float:left"><a href="http://www.soundclick.com/bands/default.cfm?bandID=1112648"><img src="http://www.soundclick.com/images/navigation/blank1x1.gif" border="0" width="150" height="170"></a></div>
+<div style="width:230px; position:relative; top:-28px; height:28px; float:left"> 
+<a href="http://www.soundclick.com/bands/default.cfm?bandID=1112648"><img src="http://www.soundclick.com/images/navigation/blank1x1.gif" border="0" width="230" height="28"></a></div></div>
+
+</div>
+</div>
+
+<div class="row">
+<div class="col-md-4 col-md-offset-4 ">
+<div class="box">
+<img class="mix" src="http://hw-img.datpiff.com/m7964cab/Tae_Money_Mg4l-front.jpg"/>
+<a href="http://www.datpiff.com/Tae-Money-Mg4l-mixtape.653024.html"><button class="b1">DOWNLOAD</button></a>
+</div></div></div>
+<div class="row" id="video">
+<div class="col-md-6 col-md-offset-3">
+<div class="box2">
+<iframe class="i" src="https://www.youtube.com/embed/mOSmGb15eA8" frameborder="0" allowfullscreen></iframe>
+<iframe class="i2" src="https://www.youtube.com/embed/SZLMCynz17U" frameborder="0" allowfullscreen></iframe>
+<h1 class="l">LATEST VIDEO</h1>
+<div class="buttons">
+<button class="a">VIDEO 1</button>
+<button class="b">VIDEO 2</button>
+
+</div>
+
+
+</div>
+</div>
+</div>
+<div class="row" id="contact">
+<div class="col-md-6 col-md-offset-3">
+<div id="form-main">
+  <div id="form-div">
+    <form class="form" id="form1" name="contactform" method="post" action="send_form_email.php">
+      
+      <p class="name">
+        <input name="name" type="text" class="validate[required,custom[onlyLetter],length[0,100]] feedback-input" placeholder="Name" id="name" />
+      </p>
+      
+      <p class="email">
+        <input name="email" type="text" class="validate[required,custom[email]] feedback-input" id="email" placeholder="Email" />
+      </p>
+      
+      <p class="text">
+        <textarea name="comments" class="validate[required,length[6,300]] feedback-input" id="comment" placeholder="Feedback"></textarea>
+      </p>
+      
+      
+      <div class="submit">
+        <input type="submit" value="Submit" id="button-blue"/>
+        <div class="ease"></div>
+      </div>
+    </form>
+  </div></div></div>
+  
+  </div>
+  
+  <div class="row">
+  <div class="col-md-12">
+  <footer><p>DEVIN SCOTT 2015</p></footer></div></div></div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<script>
+$(document).ready(function(){
+
+$(".a").click(function(){
+$(".i3, .i2").fadeOut(1); $(".i").fadeIn(1);
+$(".l").html('LATEST VIDEO');
+
+});
+
+$(".b").click(function(){
+$(".i").fadeOut(1); $(".i2").delay(1).fadeIn(1);
+$(".l").html('PRODUCTION FOR JUICY J');
+
+});
+
+$(function() {
+  $('a[href*=#]:not([href=#])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html,body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
+    }
+  });
+});
+
+});
+
+</script>
+  </body>
+</html>
